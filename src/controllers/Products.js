2d856dcbf42a027a3e587872ebd05e194b9f3dee
Kübra Özgan <86002359+KubraOzgan@users.noreply.github.com>
2021-12-19 00:57:34 +0300

    Added files

--- src/controllers/Products.js
+++ src/controllers/Products.js
@@ -14,7 +14,7 @@ const index = (req, res) => {
 };
 
 const create = (req, res) => {
-  req.body.user_id = req.user; //Birisinin icerisindeki body'e digerini ekle (req ile gelen user a esitle)
+  req.body.user_id = req.user; 
   insert(req.body)
     .then((createdDoc) => {
       if (!createdDoc)
@@ -45,7 +45,7 @@ const addComment = (req, res) => {
   if(!req.params.id){
     return res.status(httpStatus.BAD_REQUEST).send({ message: "Missing information."});
   }
-  findOne({_id: req.params.id}) //id'si req.params.id'den gelen 
+  findOne({_id: req.params.id}) 
   .then((mainProduct) => {
     if(!mainProduct) {
       return res.status(hs.NOT_FOUND).send({ message: "Not found!"});
@@ -53,10 +53,10 @@ const addComment = (req, res) => {
     const comment = {
       ...req.body,
       created_at: new Date(),
-      user_id: req.user,  //req ile gelen user dan gelecek
+      user_id: req.user, 
     };
     mainProduct.comments.push(comment);
-    updateDoc(req.params.id, mainProduct) //Eklenen yorumu ana modele de ekle. 2.yol : mainProduct.save().then()... response dondurdugumuz icin bunu tercih etmedik.
+    updateDoc(req.params.id, mainProduct) //2.yol : mainProduct.save().then()... response dondurdugumuz icin bunu tercih etmedik.
       .then((updatedDoc) => {
         if(!updatedDoc) {
           return res.status(hs.NOT_FOUND).send({ message: "Not found!"});
@@ -79,17 +79,17 @@ const addMedia = (req, res) => { //express-fileupload ile eklenen filelar req.fi
         if(!mainProduct) {
           res.status(hs.NOT_FOUND).send({ message: "Not found!"});
         }
-        const extension = path.extname(req.files.file.name); //Dosya uzantisi
-        const fileName = `${mainProduct._id?.toString()}${extension}`; //mainProducttan aldigimiz icin stringe cevir.
-        const folderPath = path.join(__dirname, "../", "uploads/products", fileName); //Bulundugumuz klasoru baz alrak yol verdik. / fileName: req.files.file.name
+        const extension = path.extname(req.files.file.name); 
+        const fileName = `${mainProduct._id?.toString()}${extension}`; 
+        const folderPath = path.join(__dirname, "../", "uploads/products", fileName); 
 
         //req'de files isimli bir kisim, onun icinde de file isimli key/property var. Onun icinde de mv var.
         req.files.file.mv(folderPath, function(err) { //file icinde "mv" isimli bir function var ve data burada tutuluyor. Yni mv yi kullanarak file i istedigimiz foldera aktarabiliriz.
           if(err) {
             return res.status(hs.INTERNAL_SERVER_ERROR).send(err);
           }
-          mainProduct.media = fileName; //Product modelindeki media ya atadik!!!
-          updateDoc(req.params.id, mainProduct) //Ana modele ekle.
+          mainProduct.media = fileName; 
+          updateDoc(req.params.id, mainProduct) 
             .then((updatedDoc) => {
               if(!updatedDoc) {
                 return res.status(hs.NOT_FOUND).send({ message: "Not found!"});
